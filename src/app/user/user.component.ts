8e142422307353069b6f1cb5dc4061256b6063ed
Sajid Manzoor <sajid.manzoor@tenpearls.com>
2025-05-26 18:44:07 +0500

    printed list of users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { isStandalone } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  standalone: false,
 })
 export class UserComponent implements OnInit {
   random:number = 0;
   selectedUser: any = '';
+  users: any[] = [];
   constructor() { 
     this.randomUser();
   }
@@ -17,16 +20,28 @@ export class UserComponent implements OnInit {
     return `assets/users/${this.selectedUser.avatar}`;
   }
 
-  onSelectUser()  {
+  onSelectUser(user:any)  {
     console.log('Selected user:');
-    this.randomUser();
+    console.log(user);
+    //this.randomUser();
   }
   randomUser() {
     this.random = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser = DUMMY_USERS[this.random]
   }
+
+  getallUsers() {
+    return DUMMY_USERS;
+  }
+  getUserById(id: string) {
+    return DUMMY_USERS.find(user => user.id === id);
+  }   
+
+  getUserbyName(name: string) {   
+    return DUMMY_USERS.find(user => user.name === name);
+  }
   ngOnInit(): void {
-    
+    this.users = this.getallUsers();
   }
 
 }
